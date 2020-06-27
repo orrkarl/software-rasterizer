5b1d9f8fb7c3a4034b3ab8c1b877a428cccb314e
orrkarl <karlor041@gmail.com>
2020-06-27 19:33:44 +0300

    began implementing obj loading

--- user_data.cpp
+++ user_data.cpp
@@ -1,5 +1,9 @@
 #include "user_data.h"
 
+#include <tinyobjloader/tiny_obj_loader.h>
+#include <map>
+#include <memory>
+
 #include "converters.h"
 #include "rasterizer.h"
 #include "util.h"
@@ -12,6 +16,15 @@ float frac(float x) {
 	return tmp >= 0.0f ? tmp : 1.0f - tmp;
 }
 
+struct Texture {
+	Texture(u8vec4* tex, u32 w, u32 h) : buffer(tex), width(w), height(h) {
+	}
+
+	std::unique_ptr<u8vec4> buffer;
+	u32 width;
+	u32 height;
+};
+
 struct FixedColorShader : MiniFragmentShader<vec3, FixedColorShader> {
 	vec4 shade(vec3 data) {
 		return {data, 1.0f};
@@ -19,89 +32,59 @@ struct FixedColorShader : MiniFragmentShader<vec3, FixedColorShader> {
 };
 
 struct Texture2DSamplerShader : MiniFragmentShader<vec2, Texture2DSamplerShader> {
-	Texture2DSamplerShader(u8vec4* tex, u32 w, u32 h) : texture(tex), width(w), height(h) {
+	Texture2DSamplerShader(Texture& tex) : texture(tex) {
 	}
 
 	vec4 shade(vec2 data) {
-		u16 s = frac(data.s) * width - 0.5f;
-		u16 t = frac(data.t) * height - 0.5f;
-		u16 idx = t * width + s;
-		return vec4(texture[s * width + t]) * (1.0f / 255);
+		u16 s = frac(data.s) * texture.width - 0.5f;
+		u16 t = frac(data.t) * texture.height - 0.5f;
+		u16 idx = t * texture.width + s;
+		auto buffer = texture.buffer.get();
+		return vec4(buffer[s * texture.width + t]) * (1.0f / 255);
 	}
 
-	u8vec4* texture;
-	u32 width;
-	u32 height;
+	Texture& texture;
 };
 
-std::vector<mat4> generateRandomTransforms() {
-	std::vector<mat4> objects;
+std::vector<vec3> g_vertecies;
+std::vector<vec2> g_texCoords;
+std::vector<u32> g_indices;
+std::map<std::string, std::unique_ptr<Texture>> g_textures;
 
-	const mat4 identity(1.f);
- 
-    auto M0 = translate(identity, glm::vec3(0, 0, 2.f));
-    M0 = rotate(M0, glm::radians(45.f), glm::vec3(0, 1, 0));
- 
-    auto M1 = translate(identity, glm::vec3(-3.75f, 0, 0));
-    M1 = rotate(M1, glm::radians(30.f), glm::vec3(1, 0, 0));
- 
-    auto M2 = translate(identity, glm::vec3(3.75f, 0, 0));
-    M2 = rotate(M2, glm::radians(60.f), glm::vec3(0, 1, 0));
- 
-    auto M3 = glm::translate(identity, glm::vec3(0, 0, -2.f));
-    M3 = rotate(M3, glm::radians(90.f), glm::vec3(0, 0, 1));
-
-    objects.push_back(M0);
-    objects.push_back(M1);
-    objects.push_back(M2);
-    objects.push_back(M3);
-
-	return objects;
-}
-
-const std::vector<vec3> g_cubeVertecies{
-    { 1.0f, -1.0f, -1.0f },
-    { 1.0f, -1.0f, 1.0f },
-    { -1.0f, -1.0f, 1.0f },
-    { -1.0f, -1.0f, -1.0f },
-    { 1.0f, 1.0f, -1.0f },
-    {  1.0f, 1.0f, 1.0f },
-    { -1.0f, 1.0f, 1.0f },
-    { -1.0f, 1.0f, -1.0f },
-};
- 
-const std::vector<std::array<uint32_t, 3>> g_cubeIndices{
-    {1, 3, 0}, 
-	{7, 5, 4}, 
-	{4, 1, 0},
-	{5, 2, 1}, 
-	{2, 7, 3}, 
-	{0, 7, 4}, 
-	{1, 2, 3}, 
-	{7, 6, 5}, 
-	{4, 5, 1}, 
-	{5, 6, 2}, 
-	{2, 6, 7}, 
-	{0, 3, 7}
-};
-
-const std::vector<vec3> g_cubeColors{
-	glm::vec3(0, 0, 0),
-	glm::vec3(0, 0, 1),
-	glm::vec3(0, 1, 0),
-	glm::vec3(0, 1, 1),
-	glm::vec3(1, 0, 0),
-	glm::vec3(1, 0, 1),
-	glm::vec3(1, 1, 0),
-	glm::vec3(1, 1, 1),
-};
-
-std::vector<mat4> g_transforms;
 mat4 g_view;
 mat4 g_proj;
 vec3 g_cameraPos(0, -8.5, -5);
 vec3 g_cameraTarget(20, 5, 1);
 vec3 g_cameraUp(0, 1, 0);	
+
+void loadScene(
+	const std::string& sceneFileName, 
+	std::vector<vec3>& vertecies, std::vector<vec2>& texCoords, std::vector<u32>& indices) {
+	tinyobj::attrib_t attribs;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	std::string warn;
+	std::string err;
+
+	auto actualFilePath = "../resources/" + sceneFileName;
+	auto status = tinyobj::LoadObj(
+		&attribs, &shapes, &materials, 
+		&warn, &err, 
+		actualFilePath.c_str(), "../resources",
+		true, true);
+
+	if (!warn.empty()) {
+		std::cerr << "TinyObj loaded with warnings:" << std::endl;
+		std::cerr << warn << std::endl;
+	}
+	if (!status) {
+		std::cerr << "TinyObj failed to load " << actualFilePath << ":" << std::endl;
+		std::cerr << err << std::endl;
+		throw std::runtime_error("TinyObj failed to load");
+	}
+
+	
+}
 
 void init(const uvec2& viewport) {
 	float nearPlane = 0.125f;
@@ -112,6 +95,8 @@ void init(const uvec2& viewport) {
 		static_cast<float>(viewport.x) / static_cast<float>(viewport.y), 
 		nearPlane, 
 		farPlane);
+
+	loadScene("sponza.obj", g_vertecies, g_texCoords, g_indices);
 }
 
 void periodic(GLFWwindow* window, const uvec2& viewport, float* depthBuffer, Color* colorBuffer) {
@@ -120,17 +105,7 @@ void periodic(GLFWwindow* window, const uvec2& viewport, float* depthBuffer, Col
 	g_view = lookAt(g_cameraPos, g_cameraTarget, g_cameraUp);
 	g_view = glm::rotate(g_view, glm::radians(-30.f), glm::vec3(0, 1, 0));
 
-	for (const auto& transform : g_transforms) {		
-		auto mvp = g_proj * g_view * transform;
-		rasterTriangleIndexed<FixedColorShader>(
-			viewport, 
-			g_cubeVertecies, 
-			g_cubeColors, 
-			g_cubeIndices, 
-			mvp, 
-			FixedColorShader(),
-			depthBuffer, 
-			colorBuffer);
-	}
+	auto mvp = g_proj * g_view;
+
 }
 
